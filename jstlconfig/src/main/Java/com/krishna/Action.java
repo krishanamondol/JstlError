b65a997c61e8b902f31ddcf1bd5941ce405e297f
Krishna Mondal <100139809+krishanamondol@users.noreply.github.com>
2022-11-14 00:47:18 +0600

    Add files via upload

---
+++ jstlconfig/src/main/Java/com/krishna/Action.java
@@ -0,0 +1,45 @@
+package com.krishna;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+//@WebServlet("/action")
+public class Action extends HttpServlet {
+    // Method to handle GET method request.
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using GET Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor = \"#f0f0f0\">\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h1> Hello World </h1>"+
+
+                "</body>"+
+                "</html>"
+        );
+    }
+
+    // Method to handle POST method request.
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        doGet(request, response);
+    }
+}
---
+++ jstlconfig/src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%--
+  Created by IntelliJ IDEA.
+  User: Krishna Mondal
+  Date: 13/11/2022
+  Time: 10 : 12 PM
+  To make a jstl code environment
+--%>
+
+<html>
+<head>
+    <title>Jstl</title>
+</head>
+<body>
+
+<!--THis c:out tag is used for displaying output-->
+<c:out value="${'Hello World.'}" />
+
+</body>
+</html>
